9c2e2c808cdc0e589b1ef0a3f955e8d0d0ee7295
Dennis <info@dengsn.com>
2018-03-07 23:20:29 +0100

    Added support for error pages (404 and 500)

--- src/Feather.php
+++ src/Feather.php
@@ -1,6 +1,7 @@
 <?php
 namespace Feather;
 
+use Exception;
 use Feather\Router\Router;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,9 @@ class Feather
 {
   // Variables
   private $pages;
-  private $default;
+  private $defaultPage;
+  private $errorPage;
+  private $notFoundPage;
   
   private $twigLoader;
   private $twigEnvironment;
@@ -23,27 +26,48 @@ class Feather
   {
     // Initialize variables
     $this->pages = [];
-    $this->default = null;
+    $this->defaultPage = null;
+    $this->errorPage = null;
+    $this->notFoundPage = null;
     
     // Initialize Twig
     $this->twigLoader = new Twig_Loader_Filesystem($path);
-    $this->twigEnvironment = new Twig_Environment($this->twigLoader,[
-      'autoescape' => false
-    ]);
+    $this->twigEnvironment = new Twig_Environment($this->twigLoader,['autoescape' => false]);
   }
   
   // Run
   public function run(array $variables = [])
   {
     // Set the default page if none given
-    if (!$this->default)
-      $this->default = array_values($this->pages)[0];
+    if (!$this->defaultPage)
+      $this->defaultPage = array_values($this->pages)[0];
     
     // Create the routes
     $router = new Router();
     $router->addRoute('/{page}',function($page) use($variables) { 
-      return $this->page($page,$variables); 
-    },['page' => $this->default->name]);   
+      try
+      {
+        return $this->renderPageByName($page,$variables); 
+      }
+      catch (NotFoundHttpException $ex)
+      {
+        if ($this->notFoundPage !== null)
+          return $this->renderPage($this->notFoundPage,$variables);
+        else
+          throw $ex;
+      }
+      catch (Exception $ex)
+      {
+        $variables = array_merge($variables,[
+          'error' => $ex->getMessage()
+        ]);
+        
+        if ($this->errorPage !== null)
+          return $this->renderPage($this->errorPage,$variables);
+        else
+          throw $ex;
+      }
+    },['page' => $this->defaultPage->name]);   
     
     // Create the request
     $request = Request::createFromGlobals();
@@ -56,16 +80,11 @@ class Feather
     $router->terminate($request,$response);
   }
   
-  // Get a page and render it
-  private function page(string $name, array $variables = [])
+  // Render a page
+  private function renderPage(Page $page, array $variables = [])
   {
     try
     {
-      // Get the page
-      $page = $this->pages[$name];
-      if (!$page)
-        throw new NotFoundHttpException($name);
-      
       // Create the Twig context variables
       $variables = array_merge($variables,[
         'pages' => $this->pages,
@@ -77,8 +96,20 @@ class Feather
     }
     catch (Twig_Error_Loader $ex)
     {
-      throw new NotFoundHttpException($name,$ex);
+      throw new NotFoundHttpException($ex->getMessage(),$ex);
     }
+  }
+  
+  // Get a page and render it
+  private function renderPageByName(string $name, array $variables = [])
+  {
+    // Get the page
+    $page = $this->pages[$name];
+    if (!$page)
+      throw new NotFoundHttpException($name);
+      
+    // Render the page
+    return $this->renderPage($page,$variables);
   }
   
   // Add a page
@@ -89,7 +120,7 @@ class Feather
     
     // Set the page as default if set
     if ($default)
-      $this->default = $page;
+      $this->defaultPage = $page;
     
     // Return self for chainability
     return $this;
@@ -100,6 +131,26 @@ class Feather
   {
     // Remove the page
     unset($this->pages[$pageName]);
+    
+    // Return self for chainability
+    return $this;
+  }
+  
+  // Set the 500 page
+  public function setErrorPage(Page $page)
+  {
+        // Set the error page
+    $this->errorPage = $page;
+    
+    // Return self for chainability
+    return $this;
+  }
+  
+  // Set the 404 page
+  public function setNotFoundPage(Page $page)
+  {
+    // Set the not found page
+    $this->notFoundPage = $page;
     
     // Return self for chainability
     return $this;
